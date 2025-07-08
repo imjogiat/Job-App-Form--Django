c46feb953813a849aeeed4da9c2e5cce4872e220
Ismail <imjogiat@gmail.com>
2025-07-08 17:09:14 -0600

    corrected error- changed the name to match html input tags

--- mysite/job_application/forms.py
+++ mysite/job_application/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class ApplicationForm(forms.Form):
     first_name = forms.CharField(max_length=80)
     last_name = forms.CharField(max_length=80)
-    email_addr = forms.EmailField()
+    email = forms.EmailField()
     date = forms.DateField()
     occupation = forms.CharField(max_length=80)
--- mysite/job_application/views.py
+++ mysite/job_application/views.py
@@ -4,21 +4,23 @@ from .forms import ApplicationForm
 
 def index(request):
     if request.method == "POST":
-        form = ApplicationForm(request.POST)
+        form = ApplicationForm(data=request.POST)
         print("application form object created")
-        print(f"Application form data {form.first_name} {form.last_name}")
+    
         #cleaned_Data is a method in the parent class forms.Form
         #that adds the user input to a dictionary and cleans the format
         print(form.is_valid())
+        print(f"\n\n{form.errors}\n\n")
+
         if form.is_valid():
             print("reached form")
             first_name = form.cleaned_data["first_name"]
             last_name = form.cleaned_data["last_name"]
-            email_addr = form.cleaned_data["email"]
+            email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
         
-        print(f"{first_name}")
+            print(f"print log: extracted form data- {first_name}")
 
     return render(request, "index.html")
 
